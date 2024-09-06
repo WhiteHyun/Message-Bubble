b61e7ed72b77323199005d7a1cbdbcd569774b06
SeungHyun Hong <whi7ehyun@gmail.com>
2024-09-06 16:41:54 +0900

    Initial Commit

---
+++ CommentBubble/CommentBubbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommentBubbleApp.swift
+//  CommentBubble
+//
+//  Created by Hyun on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct CommentBubbleApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
