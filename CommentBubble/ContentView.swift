e950b01112724c4fa296735d37e821cb961cbad3
SeungHyun Hong <whi7ehyun@gmail.com>
2024-09-07 04:43:44 +0900

    feat: TextField로 텍스트 submit 시 채팅 기록 추가

--- CommentBubble/ContentView.swift
+++ CommentBubble/ContentView.swift
@@ -8,17 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var texts: [String] = []
+  @State private var typingText: String = ""
+  init(texts: [String] = []) {
+    self._texts = State(wrappedValue: texts)
+  }
+
   var body: some View {
-    VStack {
-      Image(systemName: "globe")
-        .imageScale(.large)
-        .foregroundStyle(.tint)
-      Text("Hello, world!")
+    VStack(alignment: .leading) {
+      ForEach(texts, id: \.self) {
+        Text($0)
+      }
+      TextField("", text: $typingText)
+        .textFieldStyle(.squareBorder)
+        .onSubmit {
+          texts.append(typingText)
+          typingText = ""
+        }
+
     }
-    .padding()
+    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading)
+    .safeAreaPadding(30)
   }
 }
 
+private extension [String] {
+  static var mock: Self { ["Hello, World!", "My name is SeungHyun", "It's lorem Ipsum"] }
+}
+
 #Preview {
-  ContentView()
+  ContentView(texts: .mock)
 }
